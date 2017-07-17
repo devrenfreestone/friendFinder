200f400ead265429d4a00c4c22633daffab65457
Devren Freestone <Dev@MacBook-Pro-2.local>
2017-07-16 19:48:53 -0700

    some .js and .html files started, error on friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+console.log("friends.js linked");
+
+const questionHtml = $("#questions")
+
+//holds finalized questions to push into survey.html
+var qArray = [];
+
+//holds questions themselves
+var questions = ["Intelligence is important to me","I enjoy spending time with other people",
+	"Humor is important to me","Honesty is important to me","I enjoy cooking","I workout regularly",
+	"I enjoy sugary foods","Bacon is life","Binge watching Netflix is a valid passtime",
+	"I will be your date for ice cream whenever you want"]
+
+//populates qArray
+
+var newQuestion = () =>{
+	for (let i = questions.length - 1; i >= 0; i--) {
+		qArray.push({text: questions[i],response: [1,2,3,4,5]})
+	}
+	for(let j = 0; j<qArray.length; j++) {
+		let qNumber = j + 1;
+		console.log("Question " + qNumber + " " + qArray[j].text + "<br>" qArray[j].response);
+	}
+};
+
+newQuestion();
+
+
